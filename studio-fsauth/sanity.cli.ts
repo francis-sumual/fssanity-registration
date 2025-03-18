96e6f6a4e11a17b60ae4a8c73f8d6bd97cc69011
Francis Sumual <francis.09.03.60@gmail.com>
2025-03-19 00:28:51 +0700

    noclisanity

--- studio-fsauth/sanity.cli.ts
+++ studio-fsauth/sanity.cli.ts
@@ -1,13 +1,10 @@
-import {defineCliConfig} from 'sanity/cli'
+// import {defineCliConfig} from 'sanity/cli'
 
-export default defineCliConfig({
-  api: {
-    projectId: 's33vmf0y',
-    dataset: 'production'
-  },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
-})
+// export default defineCliConfig({
+//   api: {
+//     projectId: 's33vmf0y',
+//     dataset: 'production'
+//   },
+
+//   autoUpdates: true,
+// })
