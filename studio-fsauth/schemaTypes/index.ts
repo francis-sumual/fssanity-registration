b12392aa07bd1fc12a6711b3406d69fe3e0d3386
Francis Sumual <francis.09.03.60@gmail.com>
2025-03-19 00:38:45 +0700

    nostudio

--- studio-fsauth/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import {userType} from './userType'
-import {homeType} from './homeType'
-import {groupType} from './group'
-import {memberType} from './member'
-import {gatheringType} from './gathering'
-
-export const schemaTypes = [userType, homeType, groupType, memberType, gatheringType]
