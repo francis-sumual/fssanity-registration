5eeb69ba565434f679eb885860a5e900fcbf05dc
Francis Sumual <francis.09.03.60@gmail.com>
2025-07-14 09:43:50 +0700

    rekoleksi

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -15,6 +15,15 @@ export default async function HeroSection() {
 
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <div className="flex items-center justify-center mb-10">
+        {hero.image ? (
+          <Image src={"/backdrop.png"} alt={hero.title} width={600} height={400} className="rounded-lg object-cover" />
+        ) : (
+          <div className="relative h-[300px] w-[400px] bg-gradient-to-r from-primary/20 to-secondary/20 rounded-lg flex items-center justify-center text-muted-foreground">
+            <span className="text-lg font-medium">Hero Image</span>
+          </div>
+        )}
+      </div>
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">
@@ -36,8 +45,8 @@ export default async function HeroSection() {
               <Image
                 src={hero.image.url || "/placeholder.svg"}
                 alt={hero.title}
-                width={400}
-                height={300}
+                width={600}
+                height={400}
                 className="rounded-lg object-cover"
               />
             ) : (
